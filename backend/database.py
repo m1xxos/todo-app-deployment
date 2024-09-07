55e4c7e7aa9751c9fc0dedf2b34b720faef6d070
m1xxos <66390094+m1xxos@users.noreply.github.com>
2024-09-07 17:25:00 +0300

    feat:working db

--- backend/database.py
+++ backend/database.py
@@ -5,10 +5,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
+load_dotenv()
+
 SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
