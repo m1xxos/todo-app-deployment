ac5eaece49de52a7c355461d2081625c925106c7
m1xxos <66390094+m1xxos@users.noreply.github.com>
2024-09-09 14:07:00 +0300

    feat:user router refactoring

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,10 +2,12 @@ from fastapi import Depends, FastAPI, HTTPException, Request, Response
 from sqlalchemy.orm import Session
 from prometheus_fastapi_instrumentator import Instrumentator
 
-from . import crud, models, schemas
+# from . import crud, models, schemas
 from .database import SessionLocal, engine
+from .routers import users
+from .dependencies import get_db
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -22,65 +24,41 @@ async def db_session_middleware(request: Request, call_next):
         request.state.db.close()
     return response
 
-# Dependency
-def get_db(request: Request):
-    return request.state.db
-
-
 @app.get("/ping", status_code=200)
 async def ping():
     return {"ping": "ok"}
 
 
-@app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
+app.include_router(users.router)
 
 
-@app.get("/users/", response_model=list[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
-    return users
+# @app.post("/users/{user_id}/todos/", response_model=schemas.Todo)
+# def create_todo_for_user(
+#     user_id: int, todo: schemas.TodoCreate, db: Session = Depends(get_db)
+# ):
+#     return crud.create_user_todo(db=db, todo=todo, user_id=user_id)
 
 
-@app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
+# @app.get("/users/{user_id}/todos/", response_model=list[schemas.Todo])
+# def get_todos_for_user(user_id: int, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+#     return crud.get_todos_by_user(db, user_id, skip, limit)
 
 
-@app.post("/users/{user_id}/todos/", response_model=schemas.Todo)
-def create_todo_for_user(
-    user_id: int, todo: schemas.TodoCreate, db: Session = Depends(get_db)
-):
-    return crud.create_user_todo(db=db, todo=todo, user_id=user_id)
+# @app.get("/todos/", response_model=list[schemas.Todo])
+# def read_todos(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+#     todos = crud.get_todos(db, skip=skip, limit=limit)
+#     return todos
 
+# @app.get("/todo/{todo_id}", response_model=schemas.Todo)
+# def get_todo(todo_id: int, user_id: int, db: Session = Depends(get_db)):
+#     todo = crud.get_user_todo(db, user_id, todo_id)
+#     if todo is None:
+#         raise HTTPException(404, "Todo not found")
+#     return todo
 
-@app.get("/users/{user_id}/todos/", response_model=list[schemas.Todo])
-def get_todos_for_user(user_id: int, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    return crud.get_todos_by_user(db, user_id, skip, limit)
-
-
-@app.get("/todos/", response_model=list[schemas.Todo])
-def read_todos(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    todos = crud.get_todos(db, skip=skip, limit=limit)
-    return todos
-
-@app.get("/todo/{todo_id}", response_model=schemas.Todo)
-def get_todo(todo_id: int, user_id: int, db: Session = Depends(get_db)):
-    todo = crud.get_user_todo(db, user_id, todo_id)
-    if todo is None:
-        raise HTTPException(404, "Todo not found")
-    return todo
-
-@app.delete("/todo/{todo_id}", response_model=schemas.Todo)
-def delete_todo(todo_id: int, db: Session = Depends(get_db)):
-    todo = crud.delete_todo(db, todo_id)
-    if todo is None:
-        raise HTTPException(404, "Todo not found")
-    return todo
+# @app.delete("/todo/{todo_id}", response_model=schemas.Todo)
+# def delete_todo(todo_id: int, db: Session = Depends(get_db)):
+#     todo = crud.delete_todo(db, todo_id)
+#     if todo is None:
+#         raise HTTPException(404, "Todo not found")
+#     return todo
